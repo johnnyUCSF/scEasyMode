b2384590c0f649a6d593987039a512fe737ed4eb
JohnnyZo <johnny.yu@ucsf.edu>
2020-02-22 18:36:31 -0800

    okay pushing now

--- pymulti.py
+++ pymulti.py
@@ -164,13 +164,18 @@ def check_stats(readtable,bcsmulti,bcs10x):
     duprate = 100*(len(readtable)-len(tmp))/len(readtable)
     print("Duplication rate is: ",duprate)
     
-def filter_readtable(readtable,bcsmulti,bcs10x):
+def filter_readtable(readtable,bcsmulti,bcs10x,gbc_thresh):
     """ this filters the readtable with exact matches """
     ####subset readtable
     filtd = readtable.drop_duplicates()
     if bcsmulti != None:
         filtd = filtd[filtd.multi.isin(bcsmulti)]
     filtd = filtd[filtd['cell'].isin(bcs10x)]
+    if gbc_thesh != None:
+        ####
+        keep = filtd['multi'].value_counts()>(np.median(filtd['multi'].value_counts())*gbc_thresh)
+        keep = keep[keep==True].index.tolist()
+        filtd = filtd[filtd.multi.isin(keep)]
 #     ####check size and shrink to max
 #     max_cells = 30000
 #     keep = filtd.groupby(by='cell').count()
@@ -289,7 +294,7 @@ def correct_median(filtd,sampname,med_factor,plots=True):
 #####################main function
 #####################
 
-def pymulti(R1,R2,bcs10x,len_10x=16,len_umi=12,len_multi=8,med_factor=1.6,sampname='pymulti_',
+def pymulti(R1,R2,bcs10x,len_10x=16,len_umi=12,len_multi=8,med_factor=1.6,gbc_thresh=None,sampname='pymulti_',
             split=True,plots=True,hamming=False,thresh=False,pct_only=False,median_only=False,huge=False,bcsmulti=None,thresh_dict={}):
     """ main loop, splits from fastqs and runs through cell calls 
         R1 = your Read1 fastq for the multiseq/hashing fraction
@@ -316,6 +321,7 @@ def pymulti(R1,R2,bcs10x,len_10x=16,len_umi=12,len_multi=8,med_factor=1.6,sampna
     if split == True: reads = split_rawdata(R1,R2,len_10x,len_umi,len_multi,sampname,huge=huge)
     ###read in old pickle data
     readtable = read_pickle(sampname,reads=reads,huge=huge)
+    return(readtable)
     #####check duplication multi and 10x rates
     multirate = check_stats(readtable,bcsmulti,bcs10x)
     ####match by hamming distance
@@ -325,7 +331,7 @@ def pymulti(R1,R2,bcs10x,len_10x=16,len_umi=12,len_multi=8,med_factor=1.6,sampna
         #####check duplication multi and 10x rates
         multirate = check_stats(readtable,bcsmulti,bcs10x)
     ####filter readtable
-    filtd = filter_readtable(readtable,bcsmulti,bcs10x)
+    filtd = filter_readtable(readtable,bcsmulti,bcs10x,gbc_thresh)
     ####implement multiseq correction by within distribution zscores
     if median_only == True:
         correct_median(filtd,sampname,med_factor,plots)
