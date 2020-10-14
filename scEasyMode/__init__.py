d69811c2cc39ecad3eb3e0f4255870906174b5c9
Sohit Miglani <sohitmiglani@gmail.com>
2020-10-13 21:36:38 -0700

    removed modules and created straight import

--- scEasyMode/__init__.py
+++ scEasyMode/__init__.py
@@ -1,7 +1,1 @@
 #!/usr/bin/env python3
-
-from .modules import mousefilter
-from .modules import clusterplot
-from .modules import pymulti
-from .modules import sceasy
-from .modules import genotyping
